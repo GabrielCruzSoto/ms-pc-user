7bde99da07c816748cd5a380c795207c4330b3c6
Gabriel Alejandro Cruz Soto <gabrielcruzsoto@gmail.com>
2022-10-24 19:26:24 -0300

    Proyect Initial

---
+++ src/main/java/gcruz/photocollection/mspcuser/MsPcUserApplication.java
@@ -0,0 +1,13 @@
+package gcruz.photocollection.mspcuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPcUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPcUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gcruz/photocollection/mspcuser/MsPcUserApplicationTests.java
@@ -0,0 +1,13 @@
+package gcruz.photocollection.mspcuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPcUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
